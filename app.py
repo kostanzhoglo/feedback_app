a9c89411d900bde45dd8fecf17c016f3c305a227
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-19 13:21:50 -0400

    Start working on /submit route and function

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == 'main':
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+
+
+if __name__ == '__main__':
     # set debug to True when in development. Lets server keep reloading when we make changes.
     app.debug = True
     app.run()
