211318968c5f964f6ee9da5f14415a17c5a3e7e4
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-23 16:19:41 -0400

    Add initialization function for feedback class

--- app.py
+++ app.py
@@ -33,7 +33,12 @@ class Feedback(db.Model):
     dealer = db.Column(db.String(200))
     rating = db.Column(db.Integer(200))
     comments = db.Column(db.Text())
-    
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 # Make a route
 @app.route('/')
