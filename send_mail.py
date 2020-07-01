db45234003dbdc7b00e028af6670a796585ea478
kostanzhoglo <jeffreywithers@gmail.com>
2020-07-01 17:11:23 -0400

    Add login credentials for send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,12 +1,18 @@
 import smtplib
 from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MAIL_USER = os.getenv('MAIL_USER')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 def send_mail(customer, dealer, rating, comments):
     # port relates to mailtrap settings
     port = 2525
     smtp_server = "smtp.mailtrap.io"
-    login = ''
-    password = ''
+    login = str(MAIL_USER)
+    password = str(MAIL_PASSWORD)
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
     sender_email = "email1@example.com"
